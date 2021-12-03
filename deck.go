7a11e130776816dfdbf4581e213c06b840ff05c1
Jose Rogerio de Almeida Junior <almeidajr.vr@gmail.com>
2021-12-03 13:31:58 -0300

    Init doc

--- deck.go
+++ deck.go
@@ -12,6 +12,7 @@ const defaultDeckSize = 52
 
 type DeckOption func(Deck) Deck
 
+// NewDeck returns a new deck of cards.
 func NewDeck(opts ...DeckOption) Deck {
 	deck := make(Deck, 0, defaultDeckSize)
 
@@ -28,18 +29,21 @@ func NewDeck(opts ...DeckOption) Deck {
 	return deck
 }
 
+// WithJokers returns a new deck option with (n) jokers.
 func WithJokers(n int) DeckOption {
 	return func(d Deck) Deck {
 		return d.AddJokers(n)
 	}
 }
 
+// WithRemove returns a new deck option with cards removed.
 func WithRemove(r func(c Card) bool) DeckOption {
 	return func(d Deck) Deck {
 		return d.Remove(r)
 	}
 }
 
+// WithMany returns a deck consisting of (n) standard decks.
 func WithMany(n int) DeckOption {
 	return func(d Deck) Deck {
 		ret := make(Deck, 0, n*len(d))
@@ -52,12 +56,14 @@ func WithMany(n int) DeckOption {
 
 var shuffler = rand.New(rand.NewSource(time.Now().UnixNano()))
 
+// Shuffle shuffles the deck.
 func (d Deck) Shuffle() {
 	shuffler.Shuffle(len(d), func(i, j int) {
 		d[i], d[j] = d[j], d[i]
 	})
 }
 
+// AddJokers returns a new deck with (n) jokers.
 func (d Deck) AddJokers(n int) Deck {
 	for i := 0; i < n; i++ {
 		d = append(d, Card{Suit: Joker})
@@ -66,6 +72,7 @@ func (d Deck) AddJokers(n int) Deck {
 	return d
 }
 
+// Remove returns a new deck with cards removed.
 func (d Deck) Remove(r func(c Card) bool) Deck {
 	var ret Deck
 
